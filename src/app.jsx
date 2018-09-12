cf03d0e9d4d6768e7cd7450e75bb4fb36d19fe8a
llz721097 <1224321654@qq.com>
2018-09-12 16:57:36 +0800

    U

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import {Button} from 'antd'
-
+import axios from 'axios'
 export default class App extends React.Component {
+    componentDidMount(){
+      // axios.get(`http://localhost:8080/user/getuser/1`).then(res => {
+      //   console.log(res);
+      // });
+      console.log(process.env);
+    }
     render () {
         return (
-            <div>dsadasds
-                <div><Button type="primary">adasd</Button></div>
-            </div>
+           <div>
+             <Button type="primary">adasd</Button>
+           </div>
     )
     }
 }
